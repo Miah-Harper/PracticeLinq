73412ac9780efaeebb716268e56ec7e4e64f94ab
Miah Harper <harpermiah@yahoo.com>
2022-06-20 15:57:45 -0400

    finished linq practice

---
+++ LinqMethodsExercise/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace LinqMethodsExercise
+{
+    class Program
+    {
+        static void  Main(string[] args)
+        {
+            var videoGames = new List<string>() { "Call of Duty", "Destiny2", "Elder Scrolls", "Red Dead Redemption" };
+
+            var lengthOfVideoGames = videoGames.OrderBy(x => x.Length);
+
+           foreach(var num in lengthOfVideoGames)
+            {
+                Console.WriteLine(num);
+            }
+
+        }
+    }
+}
+
